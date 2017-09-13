8715eaf5655ba385e49a64fe5af491cf121bf145
Dominic Farquharson <farquharsondominic@gmail.com>
2017-09-12 22:46:02 -0400

    add actions: addTodo, removeTodo, ToggleTodo

---
+++ src/actions/index.js
@@ -0,0 +1,39 @@
+/*
+  Actions:
+    - payloads of informtion.
+    - sends data from application to the store.
+    - send actions using store.dispatch.
+    - plain javascript objects containing information
+    - must have a type property ('string constant').
+
+
+*/
+
+let index = 0;
+
+// adding a todo
+export const addTodo = text => {
+  // returning an object describing change to be made
+  return {
+    type: 'ADD_TODO',
+    text,
+    id: index++
+  }
+}
+
+
+// remove a to do - take todo item's id as arg
+export const removeTodo = id => {
+  return {
+    type: 'REMOVE_TODO',
+    id
+  }
+}
+
+// toggle to do - takes todo's id as arg
+export const toggleTodo = id => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
